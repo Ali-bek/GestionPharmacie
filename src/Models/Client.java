dede88ec6ec6e9f802652cde35f66d5f9f35607c
Ali-bek <66635339+Ali-bek@users.noreply.github.com>
2020-10-14 14:28:01 +0200

    First Commit

---
+++ src/Models/Client.java
@@ -0,0 +1,72 @@
+package Models;
+
+public class Client {
+
+	private String nom;
+	private String prenom;
+	private int age;
+	private String num_ss;
+	private String maladie;
+	private String traitement;
+	// contacte numero de telephone
+	
+	public Client(String nom, String prenom, int age, String num_ss, String maladie, String traitement) {
+		super();
+		this.nom = nom;
+		this.prenom = prenom;
+		this.age = age;
+		this.num_ss = num_ss;
+		this.maladie = maladie;
+		this.traitement = traitement;
+	}
+
+	public String getNom() {
+		return nom;
+	}
+
+	public void setNom(String nom) {
+		this.nom = nom;
+	}
+
+	public String getPrenom() {
+		return prenom;
+	}
+
+	public void setPrenom(String prenom) {
+		this.prenom = prenom;
+	}
+
+	public int getAge() {
+		return age;
+	}
+
+	public void setAge(int age) {
+		this.age = age;
+	}
+
+	public String getNum_ss() {
+		return num_ss;
+	}
+
+	public void setNum_ss(String num_ss) {
+		this.num_ss = num_ss;
+	}
+
+	public String getMaladie() {
+		return maladie;
+	}
+
+	public void setMaladie(String maladie) {
+		this.maladie = maladie;
+	}
+
+	public String getTraitement() {
+		return traitement;
+	}
+
+	public void setTraitement(String traitement) {
+		this.traitement = traitement;
+	}
+		
+	
+}
---
+++ src/Models/ProduitParapharmaceutique.java
@@ -0,0 +1,41 @@
+package Models;
+
+public class ProduitParapharmaceutique {
+	
+	private String type;
+	private double prix;
+	private int qte;
+	
+	public ProduitParapharmaceutique(String type, double prix, int qte) {
+		super();
+		this.type = type;
+		this.prix = prix;
+		this.qte = qte ;
+	}
+
+	public String getType() {
+		return type;
+	}
+
+	public void setType(String type) {
+		this.type = type;
+	}
+
+	public double getPrix() {
+		return prix;
+	}
+
+	public void setPrix(double prix) {
+		this.prix = prix;
+	}
+
+	public int getQte() {
+		return qte;
+	}
+
+	public void setQte(int qte) {
+		this.qte = qte;
+	}
+	
+
+}
